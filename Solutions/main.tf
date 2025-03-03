5081461e09f440de995ec3a3a84671453a6af3ce
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-03 01:56:53 -0600

    test

--- Solutions/main.tf
+++ Solutions/main.tf
@@ -1,13 +1,13 @@
-module "development_env" {
+module "development_branch" {
     source = "../Modules/oidc"
 
     identity_name = "dev-oidc-identity-01"
     repository_name = "eltelemaco/terraform-oidc"
-    entity_type = "environment"
-    environment_names = ["development"]     
+    entity_type = "ref"
+    ref_branches = ["main"]     
 }
 
 output "service_principal" {
     description = "Azure Service Principal"
-    value = module.development_env.service_principal
+    value = module.development_branch.service_principal
 }
